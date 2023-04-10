ed279b4a19f2177c1eb366c7ff3adf9916ea380c
Subham Kadam <subhamkadam09@gmail.com>
2023-04-10 15:12:46 +0530

    Structured
    typed the basic structure of the project

--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
 package Stegno_Java;
 
+class Embedder {
+
+}
+
+class Extractor {
+
+}
+
 public class Main {
-    
+    public static void main(String[] args){
+        System.out.println("Hello Boss!");
+    }
 }
