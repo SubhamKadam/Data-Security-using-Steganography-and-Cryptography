436b8bd80932d4b6c48ed51ed945c1d11be01c7f
Subham Kadam <subhamkadam09@gmail.com>
2023-04-12 13:51:37 +0530

    Extractor
    rough implementation of extractor done

--- Main.java
+++ Main.java
@@ -1,12 +1,32 @@
+import java.util.Scanner;
 import Cryptography.Encrypter;
 import Helper.*;
+import Stegnography.Extractor;
 import Data.DataWarehouse;
+
 public class Main {
     public static void main(String[] args){
         
-        DataWarehouse dataObj = new DataWarehouse();
-        InputHandler.takeInput(dataObj);
-        Converter.convertToString(dataObj);
-        Encrypter.ceaserCipher(dataObj);
+        // DataWarehouse dataObj = new DataWarehouse();
+        // InputHandler.takeInput(dataObj);
+        // Converter.convertToString(dataObj);
+        // Encrypter.ceaserCipher(dataObj);
+        
+        
+        //Embedder work - Ajay will implement
+
+        //Defining temp function for dealing with extractor(not good implementation)
+        chotaMain();
     }
+    public static void chotaMain() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter embedded image path: ");
+        String vesselImagePath = scanner.nextLine();
+        System.out.print("Enter target folder path: ");
+        String targetFolder = scanner.nextLine();
+        scanner.close();
+        Extractor extractor = new Extractor(vesselImagePath, targetFolder);
+        extractor.extract();
+        // Decrypter.ceaserCipherDecrypt();
+    } 
 }
--- Stegnography/Extractor.java
+++ Stegnography/Extractor.java
@@ -1,5 +1,79 @@
 package Stegnography;
 
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.*;
+
+import javax.imageio.ImageIO;
+
 public class Extractor {
-    
+    BufferedImage vesselImage;
+    String targetFolder;
+    String cipherText;
+    final static int HEADER_LENGHT = 20;
+    public Extractor(String vesselImagePath, String targetFolder) {
+        try {
+            vesselImage = ImageIO.read(new File(vesselImagePath)); 
+        } catch (Exception e) {
+            System.out.println("Failed to load vesselImage");
+        }
+        this.targetFolder = targetFolder;
+        Path path = Paths.get(targetFolder);
+        if (!Files.exists(path) || !Files.isDirectory(path)) {
+            System.out.println("Incorrect folder path");
+            return;
+        }
+    }
+
+    public void extract() {
+        StringBuilder header = new StringBuilder();
+        StringBuilder dataBuilder = new StringBuilder();
+        int cnt = 0;
+        for (int x = 0; x < vesselImage.getWidth(); x++) {
+            for (int y = 0; y < vesselImage.getHeight(); y++) {
+                int pixel = vesselImage.getRGB(x, y);
+                int pixelData = extractFromPixel(pixel);
+        
+                if (cnt < HEADER_LENGHT)
+                    header.append((char)pixelData);
+                else {
+                    if (cnt == HEADER_LENGHT) {
+                        //validate header
+                        //know file name and extension
+                    }
+                    else {
+                        dataBuilder.append((char)pixelData);
+                    
+                        if (cnt >= 100) {
+                            break;
+                        }    
+                        // System.out.println(pixelData);
+                    }
+                }
+                cnt++;
+            }
+        }
+        this.cipherText = dataBuilder.toString();
+        // System.out.println(this.cipherText);
+    }
+    public int extractFromPixel(int pixel) {
+        Color color = new Color(pixel, true);
+        // System.out.println(color);
+        int rBits = extractBits(color.getRed(), 3);
+        int gBits = extractBits(color.getGreen(), 3);
+        int bBits = extractBits(color.getBlue(), 2);
+        return mergeBits(rBits, gBits, bBits);
+    }
+    public int extractBits(int num, int nbits) {
+        return num&((1<<nbits) - 1);
+    }
+    public int mergeBits(int r, int g, int b) {
+        int num = (((r << 3) | g) << 2) | b;
+        return num;
+    }
+    public String getCipherText() {
+        return this.cipherText;
+    }
 }
